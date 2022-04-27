f0c227bbbe98047c331a20628f8f8ee6276c078a
flyefe <victorefe5@gmail.com>
2022-04-27 08:16:01 +0100

    cht 3 container widet

--- lib/main.dart
+++ lib/main.dart
@@ -15,8 +15,33 @@ class Homepage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: const Text("Welcome to Flutter App")),
-        body: Container(
-          child: Text("Now we are even"),
+        body: Center(
+          child: Container(
+            padding: const EdgeInsets.all(8),
+            alignment: Alignment.center,
+            height: 100,
+            width: 100,
+            decoration: BoxDecoration(
+                color: Colors.green,
+                borderRadius: BorderRadius.circular(20),
+                gradient:
+                    LinearGradient(colors: [Colors.pinkAccent, Colors.yellow]),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black12,
+                    blurRadius: 10,
+                  )
+                ]),
+            child: const Text(
+              "Learn as fast as i can",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 15,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+          ),
         ));
   }
 }
